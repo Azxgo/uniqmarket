4a7aa9dcaed062105a8b7ca191e0d618932eeae7
Azxgo <azugmoreira@gmail.com>
2026-06-10 14:37:16 -0400

    aspect ratio5

--- front-end/src/components/MiniBanners.jsx
+++ front-end/src/components/MiniBanners.jsx
@@ -20,7 +20,7 @@ export function MiniBanners() {
                         <img
                             src={minibanner.imgUrl}
                             alt={minibanner.imgAlt}
-                            className="w-full h-full object-cover
+                            className="w-full h-full
                                 transition-transform duration-300 hover:scale-105"
                                 
                         />
